1727a3c698567d43f33f16b01a28708af1305b58
juma5917 <jumasamwel422@gmail.com>
2025-04-28 17:48:48 +0300

    Fix: Use python3 in staticfiles_build.sh for Vercel

--- staticfiles_build.sh
+++ staticfiles_build.sh
@@ -5,9 +5,7 @@ set -e
 
 echo "Building static files..."
 
-# Ensure pip is up to date (optional, but can help)
-# We can often remove this too, as the python builder likely uses an up-to-date pip
-# echo "Updating pip..."
+
 # python3 -m pip install --upgrade pip
 
 # Dependencies are installed by the @vercel/python build step defined in vercel.json
